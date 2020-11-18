6eca613c181912f56c6b90cfb749e44431364d62
MitchellMarkGeorge <mitchellmmarkgeorge@gmail.com>
2020-11-17 19:46:24 -0500

    can not email link

--- background.js
+++ background.js
@@ -5,29 +5,55 @@ chrome.runtime.onStartup.addListener(createContextMenu);
 
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-  if (info && info.pageUrl && tab.title) {
-    openMailApp(tab.title, info.pageUrl);
+  // if (info && info.pageUrl && tab.title) {
+  //   openMailApp(tab.title, info.pageUrl);
+  // }
+
+  if (info.menuItemId === "emailThisPage" && info.pageUrl && tab.title) {
+    openMailApp(info.pageUrl, tab.title);
+  } else if (info.menuItemId === "emailThisLink" && info.linkUrl ) {
+    openMailApp(info.linkUrl)
   }
+
+
+
+
 });
 
 chrome.browserAction.onClicked.addListener(function (tab) { // for now - can create popup
-    console.log("here", tab);
-    if (tab && tab.url && tab.url) {
-        openMailApp(tab.title, tab.url); 
-    }
+  console.log("here", tab);
+  if (tab && tab.url && tab.url) {
+    openMailApp(tab.url, tab.title);
+  }
 })
 
-function openMailApp(title, url) {
-    let mailtoURL = "mailto:?subject=" + encodeURIComponent(title) + "&body=" + encodeURIComponent(url);
-     // tab.create()??
-    window.open(mailtoURL, "_blank");
+function openMailApp(url, title) {
+
+  let encodedURL = encodeURIComponent(url);
+  let mailtoURL = '';
+  if (title) {
+    mailtoURL = "mailto:?subject=" + encodeURIComponent(title) + "&body=" + encodedURL;
+  } else {
+    mailtoURL = "mailto:?body=" + encodedURL;
+  }
+  // mailtoURL = "mailto:?subject=" + encodeURIComponent(title) + "&body=" + encodeURIComponent(url);
+  
+  // chrome.tab.create()??
+  window.open(mailtoURL, "_blank");
 }
 
-function createContextMenu () {
-    chrome.contextMenus.create({
-      id: "emailThisPage",
-      title: "Email this page",
-      type: "normal",
-    }); // should i just call this outside of the listeners
-  
+function createContextMenu() {
+  chrome.contextMenus.create({
+    id: "emailThisPage",
+    title: "Email this page",
+    type: "normal",
+  }); // should i just call this outside of the listeners
+
+  chrome.contextMenus.create({
+    id: "emailThisLink",
+    title: "Email this Link",
+    type: "normal",
+    contexts: ["link"]
+  });
+
 }
