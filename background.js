0630db6392f322854135fa7b70e6bb7b02a983f6
Mitchell Mark-George <mitchellmmarkgeorge@gmail.com>
2022-04-17 16:11:34 -0400

    migrated to manifest version 3

--- background.js
+++ background.js
@@ -1,59 +1,53 @@
-chrome.runtime.onInstalled.addListener(createContextMenu);// will be call on install and on "updates" - is this needed
-// confrim this - should i just call createContextMenu() in global scope or should i only do so in these listeniers?
+chrome.runtime.onInstalled.addListener(createContextMenu);
 chrome.runtime.onStartup.addListener(createContextMenu);
 
-
-
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-  // if (info && info.pageUrl && tab.title) {
-  //   openMailApp(tab.title, info.pageUrl);
-  // }
-
   if (info.menuItemId === "emailThisPage" && info.pageUrl && tab.title) {
     openMailApp(info.pageUrl, tab.title);
-  } else if (info.menuItemId === "emailThisLink" && info.linkUrl ) {
-    openMailApp(info.linkUrl)
+  } else if (info.menuItemId === "emailThisLink" && info.linkUrl) {
+    openMailApp(info.linkUrl);
   }
-
-
-
-
 });
 
-chrome.browserAction.onClicked.addListener(function (tab) { // for now - can create popup
-  console.log("here", tab);
-  if (tab && tab.url && tab.url) {
-    openMailApp(tab.url, tab.title);
+chrome.action.onClicked.addListener(function (tab) {
+  if (tab && tab.title) {
+    let url;
+    // use pendingUrl if the normal url is not avalible (like the page is loading)
+    if (tab.url) {
+      url = tab.url;
+    } else {
+      url = tab.pendingUrl;
+    }
+    openMailApp(url, tab.title);
   }
-})
+});
 
 function openMailApp(url, title) {
-
   let encodedURL = encodeURIComponent(url);
-  let mailtoURL = '';
+  let mailtoURL = "";
   if (title) {
-    mailtoURL = "mailto:?subject=" + encodeURIComponent(title) + "&body=" + encodedURL;
+    mailtoURL =
+      "mailto:?subject=" + encodeURIComponent(title) + "&body=" + encodedURL;
   } else {
     mailtoURL = "mailto:?body=" + encodedURL;
   }
   // mailtoURL = "mailto:?subject=" + encodeURIComponent(title) + "&body=" + encodeURIComponent(url);
-  
-  // chrome.tab.create()??
-  window.open(mailtoURL, "_blank");
+
+  chrome.tabs.create({ url: mailtoURL });
+  // window.open(mailtoURL, "_blank");
 }
 
 function createContextMenu() {
   chrome.contextMenus.create({
     id: "emailThisPage",
-    title: "Email this page",
+    title: "Email this Page",
     type: "normal",
-  }); // should i just call this outside of the listeners
+  });
 
   chrome.contextMenus.create({
     id: "emailThisLink",
     title: "Email this Link",
     type: "normal",
-    contexts: ["link"]
+    contexts: ["link"],
   });
-
 }
